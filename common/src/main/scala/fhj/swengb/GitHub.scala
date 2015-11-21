72af353e0a5b8ea4ae4548143cc9dd564319e8d5
carinaher <carina.herzog@gmail.com>
2015-11-21 19:21:44 +0100

    added html_url and company

--- common/src/main/scala/fhj/swengb/GitHub.scala
+++ common/src/main/scala/fhj/swengb/GitHub.scala
@@ -9,7 +9,7 @@ import spray.json._
   */
 object GitHub {
 
-  case class User(login: String, avatarUrl: URL)
+  case class User(login: String, avatarUrl: URL, htmlUrl: URL, company: String)
 
   object GithubUserProtocol extends DefaultJsonProtocol {
 
@@ -17,19 +17,23 @@ object GitHub {
       def write(user: User): JsValue =
         JsArray(
           JsString(user.login),
-          JsString(user.avatarUrl.toString)
+          JsString(user.avatarUrl.toString),
+          JsString(user.htmlUrl.toString),
+          JsString(user.company.toString)
         )
 
 
       def read(value: JsValue): User = {
-        value match {
-          case JsObject(m) =>
-            val JsString(login) = m("login")
-            val JsString(a_url) = m("avatar_url")
-            User(login, new URL(a_url))
-          case x =>
-            deserializationError("GitHubUser expected.")
-        }
+          value match {
+            case JsObject(m) =>
+              val JsString(login) = m("login")
+              val JsString(a_url) = m("avatar_url")
+              val JsString(html_url) = m("html_url")
+              val JsString(company) = m("company")
+              User(login, new URL(a_url), new URL(html_url), company)
+            case x =>
+              deserializationError("GitHubUser expected.")
+          }
       }
 
     }
